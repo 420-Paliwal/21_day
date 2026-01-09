a1a0c3f555976837004b1e0ae077080527cda62f
420-Paliwal <harshpaliwal0420@gmail.com>
2026-01-09 18:50:17 +0530

    reverse digit

--- array.py
+++ array.py
@@ -5,3 +5,12 @@ class Solution:
             count += 1 
             n = n// 10
         return count
+
+class Solution:
+    def reverseNumber(self, n):
+        rev_num = 0
+        while (n > 0):
+            ld = n%10
+            n = n//10
+            rev_num = (rev_num * 10) + ld
+        return rev_num
