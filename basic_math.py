1087cbf1b9c8e45082110e6f582274a147b44360
420-Paliwal <harshpaliwal0420@gmail.com>
2026-01-09 19:35:46 +0530

    is prime  ?

--- basic_math.py
+++ basic_math.py
@@ -47,3 +47,15 @@ class Solution:
                     temp.append(n//i)
         temp.sort()
         return temp
+    
+import math
+class Solution:
+    def isPrime(self, n):
+        #your code goes here
+        if n <= 1:
+            return False 
+
+        for i in range(2, int(math.sqrt(n))+1):
+            if n%i == 0:
+                return False
+        return True
