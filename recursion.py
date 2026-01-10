4ef546311f4112e1ce2ff7a05b5cdfd4e49adc96
420-Paliwal <harshpaliwal0420@gmail.com>
2026-01-10 13:38:22 +0530

    number sum

--- recursion.py
+++ recursion.py
@@ -13,3 +13,10 @@ class Solution:
             return
         print(n)
         self.printNumbers(n-1)
+    
+class Solution:
+    def NnumbersSum(self, N):
+        #your code goes here
+        if(N<=0):
+            return 0
+        return N + self.NnumbersSum(N-1)
