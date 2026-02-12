95be7a77a898b5a143fef1c71ca8ce65ef99d9bc
420-Paliwal <harshpaliwal0420@gmail.com>
2026-02-12 13:35:25 +0530

    next greater element using stack

--- stack.py
+++ stack.py
@@ -6,10 +6,26 @@ def valid_parenthesis(s):
         ']' : '['
     }
     for ch in s:
-        if stack and ch in missing.values():
+        if ch in missing.values():
             stack.append(ch)
         else:
             if not stack and stack[-1] != missing[ch]:
                 return False
             stack.pop()
     return len(stack) == 0
+
+print(valid_parenthesis("(){}[]"))
+
+def next_greater(arr):
+    n = len(arr)
+    stack = []
+    result = [-1]*n
+    for i in range(n-1, -1, -1):
+        while stack and stack[-1] <= arr[i]:
+            stack.pop()
+        if stack:
+            result[i] = stack[-1]
+        stack.append(arr[i])
+    return result
+
+print(next_greater([4,5,2,10,8]))
