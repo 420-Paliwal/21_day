fbf358ac61873247bb4350d1914679a03589c533
420-Paliwal <harshpaliwal0420@gmail.com>
2026-02-13 18:27:56 +0530

    daily temperature element using stack

--- stack.py
+++ stack.py
@@ -84,3 +84,17 @@ def next_smaller(arr):
     return result
 
 print(next_smaller([4,8,5,2,25]))
+
+def daily_temp(arr):
+    n = len(arr)
+    stack = []
+    result = [0]*n
+
+    for i in range(n-1,-1,-1):
+        while stack and arr[stack[-1]] <= arr[i]:
+            stack.pop()
+        if stack:
+            result[i] = stack[-1] - i
+        stack.append(i)
+    
+    return result
