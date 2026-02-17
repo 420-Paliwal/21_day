a7dcaeac91ac707f70226cba5b8adf1d8fcf1eeb
420-Paliwal <harshpaliwal0420@gmail.com>
2026-02-17 23:40:35 +0530

    # Sliding window maximum optimized

--- queue.py
+++ queue.py
@@ -15,3 +15,30 @@ def sliding_window_max(arr, k):
 arr = [1,3,-1, -3, 5, 3, 6, 7]
 k = 3
 print(sliding_window_max(arr, k)) 
+
+# Sliding window maximum (OPTIMIZED APPROACH.....)
+
+from collections import deque
+
+def sliding_window_maxx_optimized(arr, k):
+    n = len(arr)
+    dq = deque()
+    res = []
+
+    for i in range(n):
+        if dq and dq[0] <= i-k:
+            dq.popleft()
+        
+        while dq and arr[dq[-1]] < arr[i]:
+            dq.pop()
+
+        dq.append(i)
+
+        if i >= k-1:
+            res.append(arr[dq[0]])
+        
+    return res
+
+arr = [1,3,-1, -3, 5, 3, 6, 7]
+k = 3
+print(sliding_window_maxx_optimized(arr, k)) 
