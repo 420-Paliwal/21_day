3cf04da832e601889e9cc35642980e2c1dce0160
420-Paliwal <harshpaliwal0420@gmail.com>
2026-02-21 19:59:46 +0530

    diameter of binary tree

--- trees.py
+++ trees.py
@@ -55,5 +55,7 @@ class TreeNode:
 root = TreeNode(1)
 root.left = TreeNode(2)
 root.right = TreeNode(3)
+root.right.left = TreeNode(6)
+root.right.right = TreeNode(7)
 root.left.left = TreeNode(4)
 root.left.right = TreeNode(5)
