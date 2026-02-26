1c40a13ae5dbd2f8ad55bc07cc11183a6e7d15bf
420-Paliwal <harshpaliwal0420@gmail.com>
2026-02-26 14:31:12 +0530

    longest consecutive in an array optimize

--- ibm_practice.py
+++ ibm_practice.py
@@ -16,3 +16,19 @@ arr = [100, 4, 200 ,1,2,3]
 print(longest_consecutive_brute(arr))
 
 def longest_consecutive_optimize(arr):
+    n = len(arr)
+    max_len = 0
+    new_set = set(arr)
+    for num in new_set:
+        if num - 1 not in new_set:
+            current = num
+            count = 1
+            while current + 1 in new_set:
+                current += 1
+                count += 1
+            
+            max_len = max(max_len, count)
+    return max_len
+
+arr = [100, 4, 200 ,1,2,3]
+print(longest_consecutive_optimize(arr))
