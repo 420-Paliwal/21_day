126908a8c159ec5f8ed3c15ec8ed3b1cdcddf277
420-Paliwal <harshpaliwal0420@gmail.com>
2026-03-09 05:21:10 +0530

    two pointer

--- ibm_practice.py
+++ ibm_practice.py
@@ -130,3 +130,12 @@ def rotate180(matrix):
     for row in matrix:
         row.reverse()
     return matrix
+
+
+def rotate270(matrix):
+    n = len(matrix)
+    for i in range(n):
+        for j in range(i, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    matrix.reverse()
+    return matrix
