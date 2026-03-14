98d536e8fc49ec196daf1b8af6e88f90d54f4d3c
420-Paliwal <harshpaliwal0420@gmail.com>
2026-03-15 03:12:28 +0530

    reverse

--- ibm_practice.py
+++ ibm_practice.py
@@ -129,10 +129,3 @@ def rotate180(matrix):
         row.reverse()
     return matrix
 
-def rotate270(matrix):
-    n = len(matrix)
-    for i in range(n):
-        for j in range(i, n):
-            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-    matrix.reverse()
-    return matrix
