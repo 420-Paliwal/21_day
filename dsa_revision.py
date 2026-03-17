2341f8cd92286792156cd8b69a59b364c7da656b
420-Paliwal <harshpaliwal0420@gmail.com>
2026-03-17 16:57:39 +0530

    factorial recusive

--- dsa_revision.py
+++ dsa_revision.py
@@ -2,4 +2,11 @@ def factorial(n):
     if n == 0 or n == 1:
         return 1
     return n * factorial(n - 1)
-print(factorial(5))
+# print(factorial(5))
+def sum_n(n):
+    if n < 0:
+        return "invalid input"
+    if n <= 1:
+        return n
+    return n + sum_n(n - 1)
+print(sum_n(5))
