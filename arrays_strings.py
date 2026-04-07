775b3de4a0966d7c8e38a80a2015980954584f19
420-Paliwal <harshpaliwal0420@gmail.com>
2026-04-07 11:07:59 +0530

    sliding window optimized

--- arrays_strings.py
+++ arrays_strings.py
@@ -77,3 +77,18 @@ class Solution(object):
                 if temp > max_avg:
                     max_avg = temp
         return max_avg
+    
+class Solution(object):
+    def findMaxAverage(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: float
+        """
+        window_sum = sum(nums[:k])
+        max_sum = window_sum
+        n = len(nums)
+        for i in range(k, n):
+            window_sum += nums[i] - nums[i-k]
+            max_sum = max(max_sum, window_sum)
+        return max_sum/float(k)
