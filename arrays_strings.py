e491f5cbbd7ed93bd690762fef2d4b640894f382
420-Paliwal <harshpaliwal0420@gmail.com>
2026-04-14 18:59:02 +0530

    permutation in an string

--- arrays_strings.py
+++ arrays_strings.py
@@ -92,3 +92,32 @@ class Solution(object):
             window_sum += nums[i] - nums[i-k]
             max_sum = max(max_sum, window_sum)
         return max_sum/float(k)
+
+
+class Solution(object):
+    def checkInclusion(self, s1, s2):
+        """
+        :type s1: str
+        :type s2: str
+        :rtype: bool
+        """
+        n1 = len(s1)
+        n2 = len(s2)
+        if n1 > n2:
+            return False
+        hash1 = {}
+        hash2 = {}
+        for i in s1:
+            hash1[i] = hash1.get(i, 0) + 1
+        left = 0
+        for right in range(n2):
+            hash2[s2[right]] = hash2.get(s2[right], 0) + 1
+            if (right-left + 1) > n1:
+                hash2[s2[left]] = hash2.get(s2[left], 0) - 1
+                if hash2[s2[left]] == 0:
+                    del hash2[s2[left]]
+                left += 1
+            if (right-left + 1) == n1:
+                if hash1 == hash2:
+                    return True
+        return False
